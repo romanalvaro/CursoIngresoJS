b7df1773b9fd069a532feb73cbf34c841f916b2d
romanalvaro <alvarotadeo.roman@outlook.com>
2020-02-13 15:26:48 -0300

    TP 1 Ferreteria completado

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -5,14 +5,16 @@ function sumar()
 {
     var dato1;
     var dato2;
-
+    var resultado
     dato1=document.getElementById("numeroUno").value;
     dato2=document.getElementById("numeroDos").value;
 
     dato1=parseInt(dato1);
     dato2=parseInt(dato2);
+    resultado=dato1+dato2;
+    //comentario, aca se suma dato1 y dato2
 
-    alert("La Suma de Es igual a " + (dato1+dato2)); 
+    alert("La Suma de Es igual a " + (dato1+dato2));
 
 }
 
--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -3,15 +3,58 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
+
 function Sumar () 
 {
-	
+    var dato1;
+    var dato2;
+    var dato3;
+    var resultado;
+
+    dato1=document.getElementById("PrecioUno").value;
+    dato2=document.getElementById("PrecioDos").value;
+    dato3=document.getElementById("PrecioTres").value;
+
+    dato1=parseInt(dato1);
+    dato2=parseInt(dato2);
+    dato3=parseInt(dato3);
+    resultado=dato1+dato2+dato3
+    alert("La Suma es igual a " + resultado);
 }
 function Promedio () 
 {
-	
+	var dato1;
+    var dato2;
+    var dato3;
+    var resultado;
+
+    dato1=document.getElementById("PrecioUno").value;
+    dato2=document.getElementById("PrecioDos").value;
+    dato3=document.getElementById("PrecioTres").value;
+
+    dato1=parseInt(dato1);
+    dato2=parseInt(dato2);
+    dato3=parseInt(dato3);
+
+    resultado=dato1+dato2+dato3/3
+    alert("La Suma es igual a " + resultado);
 }
 function PrecioFinal () 
 {
-	
+	var dato1;
+    var dato2;
+    var dato3;
+    var resultado;
+    var precioFinal;
+
+    dato1=document.getElementById("PrecioUno").value;
+    dato2=document.getElementById("PrecioDos").value;
+    dato3=document.getElementById("PrecioTres").value;
+
+    dato1=parseInt(dato1);
+    dato2=parseInt(dato2);
+    dato3=parseInt(dato3);
+    resultado=dato1+dato2+dato3
+    precioFinal=(resultado*21/100)+resultado;
+    alert("La Suma es igual a " + resultado + "Precio Final: " + precioFinal);
 }
